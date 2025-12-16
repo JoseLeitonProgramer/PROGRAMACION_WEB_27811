1f807ce92f95b06ad797d0d1cc6178019d533bb2
JoseLeitonProgramer <jileit@hotmail.com>
2025-12-16 14:50:13 -0500

    add evidence

--- TRABAJOS EN CLASE/2do PARCIAL/PRY_ARREGLOS/public/js/producto.js
+++ TRABAJOS EN CLASE/2do PARCIAL/PRY_ARREGLOS/public/js/producto.js
@@ -1,33 +1,36 @@
-var btn_agregar = document.getElementById('btn_agregar');
-var arreglos_frutas = [];
+var btn_agregar = document.getElementById("btn_agregar");
 
-btn_agregar.addEventListener('click',function(event){
-    var nombre_producto = document.getElementById('txt_nombre_producto');
-    var lista_productos = document.getElementById('lista_productos')
-    if(nombre_producto.value!=''){
-        var elementoLI = document.createElement('li');
-        elementoLI.classList.add('list-group-item');
-        elementoLI.textContent = nombre_producto.value;
+var arreglo_productos = [];
 
-        lista_productos.appendChild(elementoLI);
-        arreglos_frutas.push(nombre_producto.value)
-        nombre_producto.value = '';
+btn_agregar.addEventListener("click", function () {
+    var nombre_producto = document.getElementById("Id1");
+    var lista_productos = document.getElementById("lista_productos");
 
-    }else{
-        console.log('PORFAVOR INGRESE UN PRODUCTO')
+    if (nombre_producto.value != "") {
+        var elemento = document.createElement("li");
+        elemento.className = "list-group-item";
+        elemento.textContent = nombre_producto.value;
+
+        lista_productos.appendChild(elemento);
+        arreglo_productos.push(nombre_producto.value);
+        nombre_producto.value = "";
+        console.log(arreglo_productos);
+
+    } else {
+        console.log("INGRESE UN PRODUCTO")
     }
 
-    var productos_mayusculas = arreglos_frutas.map(function(producto){
+    var producto_mayusculas = arreglo_productos.map(function (producto) {
         return producto.toUpperCase();
-    })
+    });
 
-    console.log(productos_mayusculas)
+    var lista_productos_mayusculas = document.getElementById("lista_productos_mayusculas");
+    lista_productos_mayusculas.innerHTML = "";
+    producto_mayusculas.forEach(function (producto) {
+        var elemento = document.createElement("li");
+        elemento.className = "list-group-item";
+        elemento.textContent = producto;
+        lista_productos_mayusculas.appendChild(elemento);
+    });
 
-    var elementoLI2 = document.createElement('Li');
-    elementoLI2.classList.add('list-group-item2');
-    elementoLI2.textContent = nombre_producto.value;
-
-    lista_productos.appendChild(elementoLI2);
-    arreglos_frutas.push(nombre_producto.value)
-    nombre_producto.value = '';
-})
+});
